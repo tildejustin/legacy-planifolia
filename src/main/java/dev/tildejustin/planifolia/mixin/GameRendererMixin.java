2a347a532343661f3e5239c189d3268f5ccc7461
tildejustin <spacepepper3.14@gmail.com>
2025-07-13 12:32:43 -0400

    keep zoom off completely

--- src/main/java/dev/tildejustin/planifolia/mixin/GameRendererMixin.java
+++ src/main/java/dev/tildejustin/planifolia/mixin/GameRendererMixin.java
@@ -1,5 +1,6 @@
 package dev.tildejustin.planifolia.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.client.render.GameRenderer;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
@@ -9,6 +10,12 @@ public abstract class GameRendererMixin {
     @ModifyArg(method = "renderFog", at = @At(value = "INVOKE", target = "Lorg/lwjgl/opengl/GL11;glFogf(IF)V", ordinal = 1), slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/world/dimension/Dimension;isFogThick(II)Z")))
     private float fixNetherFog(float original) {
         return Math.min(original, 192.0F) * 0.5F;
+    }
+
+    @Dynamic
+    @ModifyExpressionValue(method = "getFov", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/KeyBinding;getCode()I", ordinal = 0))
+    private int keepZoomOff(int original) {
+        return 0;
     }
 
     @Dynamic
