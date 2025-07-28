f8995abae61223cfd25ecb89d0483f3c91be0be9
tildejustin <spacepepper3.14@gmail.com>
2025-07-28 13:29:06 -0400

    dark sky isn't rendered when vbos are on

--- src/main/java/dev/tildejustin/planifolia/mixin/WorldRendererMixin.java
+++ src/main/java/dev/tildejustin/planifolia/mixin/WorldRendererMixin.java
@@ -3,7 +3,7 @@ package dev.tildejustin.planifolia.mixin;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.client.render.WorldRenderer;
 import org.spongepowered.asm.mixin.*;
-import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.*;
 
 @Mixin(WorldRenderer.class)
 public abstract class WorldRendererMixin {
@@ -11,5 +11,10 @@ public abstract class WorldRendererMixin {
     @ModifyExpressionValue(method = "getEntitiesDebugString", at = @At(value = "INVOKE", target = "LConfig;getVersionDebug()Ljava/lang/String;", remap = false))
     private String simplifyOptiFineVersionText(String original) {
         return original.substring(original.indexOf("_", original.indexOf("_") + 1) + 1);
+    }
+
+    @Redirect(method = "renderSky", at = @At(value = "FIELD:LAST", target = "Lnet/minecraft/client/render/WorldRenderer;vbo:Z"))
+    private boolean checkVBOs(WorldRenderer instance) {
+        return false;
     }
 }
