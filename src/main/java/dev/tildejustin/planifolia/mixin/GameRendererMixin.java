52e24bd96d0d689bec03439cacc34b660c6338a1
tildejustin <spacepepper3.14@gmail.com>
2025-07-28 12:59:57 -0400

    use vanilla clip distance

--- src/main/java/dev/tildejustin/planifolia/mixin/GameRendererMixin.java
+++ src/main/java/dev/tildejustin/planifolia/mixin/GameRendererMixin.java
@@ -1,6 +1,7 @@
 package dev.tildejustin.planifolia.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.*;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.render.GameRenderer;
 import org.spongepowered.asm.mixin.*;
@@ -8,6 +9,9 @@ import org.spongepowered.asm.mixin.injection.*;
 
 @Mixin(GameRenderer.class)
 public abstract class GameRendererMixin {
+    @Shadow
+    private float viewDistance;
+
     @Unique
     private static final Object object = new Object();
 
@@ -27,4 +31,20 @@ public abstract class GameRendererMixin {
     @Redirect(method = "renderGui", at = @At(value = "INVOKE", target = "Lnet/minecraft/class_524;showLagometer(JJ)V", remap = false))
     private void disableLagMeter(GameRenderer instance, long a, long b) {
     }
+
+    @Dynamic
+    @WrapOperation(method = "setupCamera", at = {@At(value = "INVOKE", target = "LConfig;isFogFancy()Z"), @At(value = "INVOKE", target = "LConfig;isFogFast()Z")})
+    private boolean noFogCulling(Operation<Boolean> operation) {
+        return false;
+    }
+
+    @ModifyArg(method = {"setupCamera", "renderHand"}, at = @At(value = "INVOKE", target = "Lorg/lwjgl/util/glu/Project;gluPerspective(FFFF)V"), index = 3)
+    private float correctClipDistance(float original) {
+        return this.viewDistance * 2;
+    }
+
+    // @ModifyArg(method = "renderWorld", at = @At(value = "INVOKE", target = "Lorg/lwjgl/opengl/GL11;glAlphaFunc(IF)V", ordinal = 0))
+    // private float higherAlphaCutoff(float func) {
+    //         return 0.5f;
+    // }
 }
